7609a9e0947cf2a53486a14569237f39a49a789f
Petar Veličković <pv273@cam.ac.uk>
2017-03-01 22:08:16 +0000

    Evaluator pushed

--- adversarial.py
+++ adversarial.py
@@ -1,5 +1,13 @@
 from keras.applications.resnet50 import ResNet50
+from keras.layers import Input
 
 model = ResNet50(weights='imagenet', include_top=True)
+
+img_h = 224
+img_w = 224
+
+inp = model.inputs[0]
+out = model.outputs[0]
+
 
 
---
+++ evaluator.py
@@ -0,0 +1,25 @@
+# A helper class, that allows getting the loss and
+# gradients separately, without having to call the
+# getter function twice.
+
+class Eval(object):
+    def __init__(self, f):
+        self.l = None
+        self.g = None
+        self.f = f
+
+    def loss(self, x):
+        assert self.l is None
+        l, g = self.f(x)
+        self.l = l
+        self.g = g
+        return self.l
+
+    def grads(self, x):
+        assert self.l is not None
+        g = np.copy(self.g)
+        self.l = None
+        self.g = None
+        return g
+
+
