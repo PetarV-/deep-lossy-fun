e2d7249c71f039e689b75e6e5310e7169f41f542
Petar Veličković <pv273@cam.ac.uk>
2017-03-01 23:14:06 +0000

    Adv example generated :)

--- adversarial.py
+++ adversarial.py
@@ -2,6 +2,7 @@ from keras.applications.resnet50 import ResNet50, decode_predictions
 from keras.layers import Input
 from keras import backend as K
 from processing import load_and_process, deprocess_and_save
+import numpy as np
 
 # Fetch the pretrained ResNet-50
 model = ResNet50(weights='imagenet', include_top=True)
@@ -15,17 +16,28 @@ img_d = 3
 inp = model.inputs[0]
 out = model.outputs[0]
 
+cid = 89 # Class 89: Sulphur-crested Cockatoo :)
+
 # Define the loss as the negative confidence in the chosen class
 loss = K.variable(0.)
-loss -= out[:,409] # class 409: Sulphur-crested Cockatoo :)
+loss -= out[:, cid]
 
 # A function that will give us the gradients wrt the input
-f = K.function([inp], [K.gradients(loss, inp)])
+f = K.function([inp, K.learning_phase()], K.gradients(loss, inp))
 
-# FGSM: Take one step in the direction of eps * sgn(grad)
-def fgsm(x, eps=0.03):
-    grads = f([x]).reshape((1, img_h, img_w, img_d)).astype('float64')
-    return x - eps * grads
+# FGSM: Take some steps in the direction of eps * sgn(grad)
+def fgsm(x, eps=32, alp=1.0):
+    num_iter = min(eps + 4, 1.25 * eps)
+    conf = 0.0
+    while conf < 0.99 and num_iter > 0:
+        grads = np.array(f([x, 0])).reshape((1, img_h, img_w, img_d)).astype('float64')
+        adv_x = x - alp * np.sign(grads)
+        sub_x = np.minimum(x + eps, np.maximum(x - eps, adv_x))
+        next_x = np.clip(sub_x, 0.0 , 255.0)
+        x = next_x
+        conf = model.predict(x)[0, cid]
+        print('Confidence:', conf)
+    return x
 
 img = load_and_process('elephant.jpg', target_size=(img_h, img_w))
 preds = model.predict(img)
